012d0bff798fa7d3b4b713a5711e4a89a010bbae
Felix Pojtinger <felix@pojtinger.com>
2018-02-08 18:29:09 +0100

    Cleaned up config object, added graphql and graphiql endpoints

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,15 @@ import IrasyncBackend from './main'
 // Create the root instance with a configuration object
 let irasyncBackend = new IrasyncBackend({
   // API connection details
-  apiPort: '3000',
-  apiEndpoint: '/api',
-
-  // Debug config
-  // debugEndpoint: '/debug',
-
+  api: {
+    port: '3000',
+    endpoint: '/api'
+  },
+  // Debugging interface definition
+  debug: {
+    enabled: true,
+    endpoint: '/debug'
+  }
   // Database config
   // dbHost: 'http://localhost',
   // dbName: 'irasync'
--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,43 @@ import * as express from 'express';
 // Enable query parsing
 import * as bodyParser from 'body-parser';
 // Use the apollo server for graphql
-import { graphqlExpress } from 'apollo-server-express';
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 
 /**
- * Configuration object interface
+ * Main config parameters
  */
-interface ConfigObject {
+interface Params {
+  api: ApiConfig,
+  debug: DebugConfig,
+}
+
+/**
+ * The API's config parameters
+ */
+interface ApiConfig {
   // The port that the API should use (i.e. '3000')
-  apiPort: String,
+  port: String;
   // The enpoint that the API should use (i.e. '/graphql')
-  apiEndpoint: String
+  endpoint: String;
+}
+
+/**
+ * GraphiQL's config parameters (debugging and testing)
+ */
+interface DebugConfig {
+  // Whether graphiql (for debugging) should be enabled
+  enabled: Boolean,
+  // The graphiql (debugging) endpoint
+  endpoint: String
+}
+
+/**
+ * All of the available endpoints
+ */
+interface Endpoints {
+  api: String,
+  debugEnabled: Boolean,
+  debug: String
 }
 
 /**
@@ -23,15 +50,42 @@ class IrasyncBackend {
   app: any;
   EXPRESS_PORT: String;
 
-  constructor(configObject: ConfigObject) {
+  constructor(Params: Params) {
     // Create new express instance
     this.app = express();
     // Config the server
-    this.config(configObject.apiPort);
+    this.config(Params.api.port);
     // Setup the endpoints
-    this.endpoints(configObject.apiEndpoint);
+    this.setupEndpoints({
+      api: Params.api.endpoint,
+      debugEnabled: Params.debug.enabled,
+      debug: Params.debug.endpoint
+    });
     // Listen at EXPRESS_PORT
-    this.listen();
+    this.listen(this.EXPRESS_PORT);
+  }
+
+  /**
+   * Setup endpoints.
+   */
+  setupEndpoints(endpoints: Endpoints): void {
+    // Default endpoint to guid the user to the right ones
+    this.app.get('/', (req, res) => {
+      res.send(`
+You\'ve reached an Irasync API server.
+Point your browser to <a href="${endpoints.debug}">${endpoints.debug}</a> to debug or <a href="${endpoints.api}">${endpoints.api}</a> to use it.`);
+    });
+
+    // The API endpoint
+    this.app.use(endpoints.api, bodyParser.json(), graphqlExpress({}));
+
+    // The debug (GraphiQL) endpoint should only be created if it was enabled
+    if (endpoints.debugEnabled) {
+      this.app.use(endpoints.debug, graphiqlExpress({
+        // Pass the api's endpoint
+        endpointURL: endpoints.api.toString()
+      }))
+    }
   }
 
   /**
@@ -43,24 +97,10 @@ class IrasyncBackend {
   }
 
   /**
-   * Setup endpoints.
+   * Listen at the configurated ports.
    */
-  endpoints(apiEndpoint: String): void {
-    // Default endpoint to guid the user to the right ones
-    this.app.get('/', (req, res) => {
-      res.send(`
-You\'ve reached an Irasync API server.
-Point your browser to <a href="/graphiql">/graphiql</a> to debug or <a href="/graphql">/graphql</a> to use it.`);
-    });
-    // The API endpoint
-    this.app.use(apiEndpoint, bodyParser.json(), graphqlExpress({}));
-  }
-
-  /**
-   * Listen at the configured ports.
-   */
-  listen(): void {
-    this.app.listen(this.EXPRESS_PORT, () => {
+  listen(port: String): void {
+    this.app.listen(port, () => {
       console.log(`
 SUCCESS >>> IRASYNC API ONLINE <<< SUCCESS
 API URL: http://localhost:${this.EXPRESS_PORT}
