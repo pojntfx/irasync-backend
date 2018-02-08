b976abec6e3dc0875b9cdfccb0da549cb088d551
Felicitas Pojtinger <felicitas@pojtinger.com>
2018-02-08 16:00:12 +0100

    Install graphql, edit application structure

--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,6 @@
-// Use the express framework
-import * as express from 'express'
+// Use the Irasync backend server
+import IrasyncBackend from './main'
 
-/**
- * Root of a new Irasync instance
- */
-class IrasyncBackend {
-
-  app: express.Application;
-  EXPRESS_PORT: String;
-
-  constructor() {
-    // Create new express instance
-    this.app = express();
-    // Config the server
-    this.config();
-    // Setup the routes
-    this.routes();
-    // Listen at EXPRESS_PORT
-    this.listen();
-
-  }
-
-  /**
-   * Config the server.
-   */
-  config() {
-    // The port that express will run at
-    this.EXPRESS_PORT = '3000';
-  }
-
-  /**
-   * Setup routes.
-   */
-  routes() {
-    this.app.get('/', (req, res) => {
-      res.send('Hello, world!');
-    })
-  }
-
-  /**
-   * Listen at the configured ports.
-   */
-  listen() {
-    this.app.listen(this.EXPRESS_PORT, () => {
-      console.log(`
-      SUCCESS >>> IRASYNC API ONLINE <<< SUCCESS
-      Point your browser to http://localhost:${this.EXPRESS_PORT}
-      `);
-    })
-  }
-}
-
+// Create the root instance
+// The express port, mongodb port etc. will be passed in here in the future as a configuration object
 let irasyncBackend = new IrasyncBackend();
