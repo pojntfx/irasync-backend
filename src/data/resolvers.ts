41b6989d926be82c3900a9a8859b2297e8b1aa94
Felix Pojtinger <felix@pojtinger.com>
2018-02-09 19:22:21 +0100

    Added prototype of new schema

--- src/data/resolvers.ts
+++ src/data/resolvers.ts
@@ -17,6 +17,7 @@ export class Resolvers {
   // The mocked posts data
   private postsData: IPost[];
   private resolvers: any;
+  private users: IUser[];
 
   constructor() {
     // Add the mock data
@@ -68,6 +69,33 @@ export class Resolvers {
         posts: (): object[] => {
           return this.postsData;
         },
+        // Return a user by ID
+        user: (root, { id, name }): IUser => {
+          // ID has been provided, name not
+          if (id != null && name == null) {
+            // Return the firt user with specified id
+            return this.users.filter((user) => {
+              return user.meta.id === id;
+            })[0];
+          // Name has been provided, id not
+          } else if (name != null && id == null) {
+            // Return the first user with specified name
+            return this.users.filter((user) => {
+              return user.meta.name === name;
+            })[0];
+          // Both a name and an ID have been provided
+          } else if (name != null && id != null) {
+            // Throw error because the provided ID does not have to match the name provided
+            throw new Error("Both a name and an id have specified, specifiy only one.");
+          // Neither a name nor an ID have been provided
+          } else {
+            throw new Error("Neither a name nor an id has been specified.");
+          }
+        },
+        // Return a userArray of all users
+        users: (): IUser[] => {
+          return this.users;
+        },
       },
     };
   }
@@ -100,5 +128,126 @@ export class Resolvers {
       url: "https://train3.com",
       username: "pojntfx3",
     }];
+    // Mock user data
+    // tslint:disable:object-literal-sort-keys REMOVE THIS AFTER PROTOTYPING
+    this.users = [{
+      meta: {
+        id: 0,
+        name: "pojntfx",
+        firstName: "Felix",
+        lastName: "Pojtinger",
+      },
+      posts: [{
+        meta: {
+          id: 0,
+          votes: 8,
+        },
+        body: {
+          title: "A quick test post",
+          description: "A quick test description",
+        },
+        comments: [
+          {
+            meta: {
+              id: 0,
+              votes: 16,
+            },
+            body: {
+              title: "A quick test comment",
+              description: "A quick comment test description",
+            },
+          },
+        ],
+      }],
+      communities: [{
+        meta: {
+          id: 0,
+          subs: 32,
+          externalMods: [
+            {
+              meta: {
+                id: 1,
+                name: "maxmustermann",
+                firstName: "Max",
+                lastName: "Mustermann",
+              },
+            },
+          ],
+        },
+      }],
+    }];
   }
+}
+
+/**
+ * A user. Every element that is not a user itself must be owned by a user.
+ */
+interface IUser {
+  // Meta
+  meta: IUserMeta;
+  // Posts (Optional, in the beginning a user does have no post)
+  posts?: IPost2[];
+  // Communities (Optional, a user might not have a community)
+  communities?: ICommunity[];
+}
+
+/**
+ * A post. Can be both a community's post or nested post (comment).
+ */
+interface IPost2 {
+  // Meta
+  meta: IPostMeta;
+  // Content
+  body: IPostBody;
+  // Comments (A post does not have to have a comment)
+  comments?: IPost2[]; // There will only be references to the ObjectIDs here
+}
+
+/**
+ * A community. Is owned by a user.
+ */
+interface ICommunity {
+  // Meta
+  meta: ICommunityMeta;
+  // Posts (Optional, a community does not have to have any content)
+  posts?: IPost2[];
+  // Files (Optional, a community does not have to have any content)
+  files?: IFile[];
+  // Apps (Optional, a community does not have to have any content)
+  apps?: IApp[];
+  // Wiki (Optional, a community does not have to have any content)
+  wiki?: IPost2[]; // This can be nested into categories with specific names & meta tags
+}
+
+interface IApp {
+  // TODO: Add app specs (Run arbitrary js code in specified scope)
+  id: number; // We will get this from MongoDB's ObjectID
+}
+
+interface IFile {
+  // TODO: Add file specs (IPFS?)
+  id: number; // We will get this from MongoDB's ObjectID
+}
+
+interface ICommunityMeta {
+  id: number; // We will get this from MongoDB's ObjectID
+  subs: number; // No votes here because you should rather judge the content
+  externalMods?: IUser[]; // External moderators (optional), will be added by their ObjectID (only 1 can own community)
+}
+
+interface IUserMeta {
+  id: number; // We will get this from MongoDB's ObjectID
+  name: string;
+  firstName: string;
+  lastName: string;
+}
+
+interface IPostMeta {
+  id: number; // We will get this from MongoDB's ObjectID
+  votes: number;
+}
+
+interface IPostBody {
+  title: string;
+  description: string;
 }
