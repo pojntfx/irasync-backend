bd30d2a87b9f4f389c24b0734328341b2d680bba
Felicitas Pojtinger <felicitas@pojtinger.com>
2018-02-10 18:09:53 +0100

    Install sequelize, setup database connection, refactor startup

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import { IrasyncBackend, IStartParams } from "./start";
 const irasyncBackend = new IrasyncBackend({
   apiEndpoint: "/graphql",
   apiPort: 3000,
+  dbName: "irasync_backend",
+  dbPassword: "postgres",
+  dbUserName: "postgres",
   graphiqlEnabled: true,
   graphiqlEndpoint: "/graphiql",
 });
