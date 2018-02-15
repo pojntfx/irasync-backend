735f746a354a4f7b27335e7b9b72b76223837425
Felicitas Pojtinger <felicitas@pojtinger.com>
2018-02-16 00:09:29 +0100

    Added comments, interfaces and updated docs

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -3,6 +3,9 @@ import { auth } from "./Mutation/auth";
 import { post } from "./Mutation/post";
 import { Query } from "./Query";
 
+/**
+ * Export all the resolvers for use by Prisma
+ */
 export default {
   AuthPayload,
   Mutation: {
--- src/utils/logging.ts
+++ src/utils/logging.ts
@@ -6,6 +6,10 @@ import {
   LogLevel,
 } from "typescript-logging";
 
+// Normal logs should be infos
 CategoryServiceFactory.setDefaultConfiguration(new CategoryConfiguration(LogLevel.Info));
 
+/**
+ * Provide a logging category for events happening in the startup class
+ */
 export const catStartup = new Category("startup");
