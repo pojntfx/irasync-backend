9ed4eec0753f07b25e67d4dc511fdec9aded4a9b
Felicitas Pojtinger <felicitas@pojtinger.com>
2018-02-15 22:27:12 +0100

    Reimplemented pre-prisma folder structure with new prisma version, ixed typos in docs

--- prisma/irasync-backend/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { IrasyncBackend, IStartParams } from "./start";
-
-// Parameters are configured in the .env file
-// Will be passed in here in the future
-const irasyncBackend = new IrasyncBackend({
-  apiEndpoint: process.env.PRISMA_ENDPOINT,
-  secret: process.env.PRISMA_SECRET,
-});
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
 import { IrasyncBackend, IStartParams } from "./start";
 
+// Parameters are configured in the .env file
+// Will be passed in here in the future
 const irasyncBackend = new IrasyncBackend({
-  apiEndpoint: "/graphql",
-  apiPort: 3000,
-  dbName: "irasync_backend",
-  dbPassword: "postgres",
-  dbUserName: "postgres",
-  graphiqlEnabled: true,
-  graphiqlEndpoint: "/graphiql",
+  apiEndpoint: process.env.PRISMA_ENDPOINT,
+  secret: process.env.PRISMA_SECRET,
 });
